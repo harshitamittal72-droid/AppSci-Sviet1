53f0b899c36ff0fa2724641ad33f670e7a819dec
mausamazing <mausamkaushal108@gmail.com>
2025-11-19 13:39:29 +0530

    gallery backend finished

--- sviet/serve/urls.py
+++ sviet/serve/urls.py
@@ -5,15 +5,9 @@ from .views import gallery_index, gallery_category, gallery_page
 urlpatterns = [
     path('blogs/',views.blog_list, name='blog_list'),
     path('blogs/<int:blog_id>/', views.blog_detail,name="blog_detail"),
-    path("gallery/",gallery_page,name="gallery_page"),
-]
-# serve/urls.py
-from django.urls import path
-
-
-urlpatterns = [
-    # ... other serve urls ...
     path('gallery/', gallery_index, name='gallery_index'),
+    # ... other serve urls ...
+
     path('gallery/uncategorized/', gallery_category, name='gallery_uncategorized'),
     path('gallery/<slug:category_slug>/', gallery_category, name='gallery_category'),
 ]
